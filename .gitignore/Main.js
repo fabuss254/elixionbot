16d1161b910d885437ec33fbdd6567fbd6042a2b
fabuss254 <fabuss254@outlook.fr>
2018-06-15 23:36:06 +0200

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -98,11 +98,11 @@ bot.on("message", async function(message) {
                 if (args[1]){
                     if(args[1].toLowerCase() === "buildeur"){
                         if (args[2]){
-                            if(args[2].toLowerCase() === "Apprenti"){
+                            if(args[2].toLowerCase() === "apprenti"){
                                 RoleGive(message.author, "457282776903843860");
-                            }else if(args[2].toLowerCase() === "Normal"){
+                            }else if(args[2].toLowerCase() === "normal"){
                                 RoleGive(message.author, "424572882115624961");
-                            }else if{args[2].toLowerCase() === "Verifié"){
+                            }else if{args[2].toLowerCase() === "verifie"){
                                 message.channel.send("Repondez a ce formulaire, un verificateur va vous donner le role si vous êtes vraiment un buildeur: ");
                             }else{
                                 var dt_embed = new Discord.RichEmbed()
@@ -133,7 +133,7 @@ bot.on("message", async function(message) {
                         .setColor("#FFFFFF")
                         .setAuthor("Createur Fabuss254#9232")
                         .addField("Listes des rôles", "Buildeur\nModélisateur\nScripteur\nUI/UX designer\nAnimateur")
-                        .addField("Types de rôles", "Apprenti | Normal | Verifié")
+                        .addField("Types de rôles", "Apprenti | Normal | Verifie")
                         .addField("Syntaxe", ".rank <Nom du rôle> <Type du rôle>");
                     message.channel.send(dt_embed);
                 };
