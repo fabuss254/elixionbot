5813a54dafd566277cd8f30642b1a7d024f35108
fabuss254 <fabuss254@outlook.fr>
2018-07-01 20:09:28 +0200

    Update Main.js

--- .gitignore/Main.js
+++ .gitignore/Main.js
@@ -494,10 +494,10 @@ function SondageGiv(){
                     var Str = ""
                     gg.forEach(function(v,i){
                         if (gg[i+1] === "**min**"){
-                            gg[i] = math.floor((args[0] - Date.now())/60000)
-                            Str = Str + gg[i]
+                            var y = math.floor((args[0] - Date.now())/60000)
+                            Str = Str + " " + y
                         }else{
-                            Str = Str + v
+                            Str = Str + " " + v
                         };
                     });
                     msgs.get(args[1]).edit(Str);
